a09251b565c8163f007f2a0797844ca5a68a5e70
Alex <alexanderperepechaev@gmail.com>
2019-06-25 21:39:24 +0300

    Весь код перенесен в функции Также добавлена функция для внесения не обязательных расходов

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-let money = +prompt("Ваш бюджет на месяц?", ''),
-	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+let money, time;
+
+
+function start() {
+     money = +prompt("Ваш бюджет на месяц?", '');
+     time = prompt('Введите дату в формате YYYY-MM-DD', '');
+     
+     while(isNaN(money) || money == null || money == ""){
+          money = +prompt("Ваш бюджет на месяц?", '');
+     }
+}
+
+start();
 
 let appData = {
 	budget: money,
-	expenses: {},
+     expenses: {},
+     optionalExpenses: {},
 	optionalExpenses: {},
 	income: [],
 	timeData: time,
@@ -12,27 +24,56 @@ let appData = {
 
 
 
-for(let i = 0; i < 2; i++){
-     let  a = prompt("Введите обязательную статью расходов в этом месяце"),
-          b = +prompt("Во сколько обойдется?");
-          if(typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a!= '' && b!= '' && a.length < 50){
-               console.log('done');
-               appData.expenses[a] = b;
-          } else {
-               i--;
-          }
-          
-}
-appData.moneyPerDay = appData.budget/30;
+chooseExpenses();
+chooseOptExpenses();
+detectDayBudget();
+detectLevel();
 
-alert(appData.moneyPerDay);
-
-if(appData.moneyPerDay < 100) {
-     console.log('Минимальный')
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 1000){
-     console.log('Normal');
-} else if (appData.moneyPerDay > 1000){
-     console.log('Big');
-} else {
-     console.log('error');
+function chooseExpenses(){
+     for(let i = 0; i < 2; i++){
+          let  a = prompt("Введите обязательную статью расходов в этом месяце"),
+               b = +prompt("Во сколько обойдется?");
+               if(typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a!= '' && b!= '' && a.length < 50){
+                    console.log('done');
+                    appData.expenses[a] = b;
+               } else {
+                    i--;
+               }
+               
+     }
 }
+
+function chooseOptExpenses(){
+     for(let i = 0; i < 3; i++){
+          let  a = prompt("Статья необязательных расходов?");
+               if(typeof(a) === 'string' && typeof(a) != null  && a!= ''&& a.length < 50){
+                    console.log('done');
+                    appData.optionalExpenses[i] = a;
+               } else {
+                    i--;
+               }
+               
+     }
+}
+
+function detectDayBudget(){
+     appData.moneyPerDay = appData.budget/30;
+     alert(appData.moneyPerDay);
+}
+
+function detectLevel(){
+     if(appData.moneyPerDay < 100) {
+          console.log('Минимальный')
+     } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 1000){
+          console.log('Normal');
+     } else if (appData.moneyPerDay > 1000){
+          console.log('Big');
+     } else {
+          console.log('error');
+     }
+}
+
+
+
+
+
