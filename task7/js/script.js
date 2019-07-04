03ff46ca17593f5cadbea4e424ebcb7f6542cea5
Alex <alexanderperepechaev@gmail.com>
2019-07-04 12:29:06 +0300

    Сделала 7 задание по udemy JS

---
+++ task7/js/script.js
@@ -0,0 +1,24 @@
+let buttom = document.getElementById("start");
+    budgetValue = document.getElementsByClassName('.budget-value');
+    daybudgetValue = document.getElementsByClassName('.daybudget-value');
+    levelValue = document.getElementsByClassName('.level-value');
+    expensesValue = document.getElementsByClassName('.expenses-value');
+    optionalexpensesValue = document.getElementsByClassName('.optionalexpenses-value');
+    incomeValue = document.getElementsByClassName('.income-value');
+    monthsavingsValue = document.getElementsByClassName('.monthsavings-value');
+    yearsavingsValue = document.getElementsByClassName('.yearsavings-value');
+    inputField = document.getElementsByClassName('.expenses-item');
+    expensesItemBtn = document.getElementsByClassName('.expenses-item-btn');
+    optionalexpensesBtn = document.getElementsByClassName('.optionalexpenses-btn');
+    countBudgetBtn = document.getElementsByClassName('.count-budget-btn');
+    optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item');
+    chooseIncome = document.querySelector('.choose-income');
+    savings = document.querySelector('.savings');
+    sum = document.querySelector('.sum');
+    percent = document.querySelector('.percent');
+    year = document.querySelector('.year-value');
+    month = document.querySelector('.month-value');
+    day = document.querySelector('.day-value');
+
+
+console.log(year);
